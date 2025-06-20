b344a31a43fa4c7d0a085aab1cb706344fe52f0b
SebastianVernis <sebastianvernis@gmail.com>
2025-06-20 17:57:22 -0600

    Add .next/static/chunks/src_app_page_tsx_c0237562._.js 1750463841797

---
+++ .next/static/chunks/src_app_page_tsx_c0237562._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_8785aa58._.js"
+],
+    source: "dynamic"
+});
